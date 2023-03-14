35cf3aa0c0557618cc9d263472ca04abe466a64f
luisSantamariaCol <luisestebansantamaria@outlook.com>
2023-03-14 09:26:29 -0500

    search_result template added

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     name = request.GET.get("name") or "world" # if name is None, use "world"
-    return HttpResponse(f"Hello, {name}. You're at the reviews index.")
+    
+    return render(request, "base.html", {"name": name})
